9e2d24d0210486fdcd1d33e775c1c45040058f79
名前 <>
2020-09-21 13:24:33 +0000

    投稿機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/ }
     
     has_secure_password
+    
+    has_many :topics
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
+  get 'sessions/new'
+  
   root 'pages#index'
   get 'pages/help' # pagescontrollerの中にあるhelpアクションをゲットして、helpアクションに該当するページを呼び出す という意味合い
   get '/preview', to: 'pages#preview' # このように書けば、URLのところの/pages/previewっていうのを/previewだけに省略できる。
 
   resources :users
+  resources :topics
   
   get     '/login',  to: 'sessions#new'
   post    '/login',  to: 'sessions#create'
