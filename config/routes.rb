82cc4f49ceff9622a0b5c4c73efb7106d79ef910
名前 <>
2020-09-22 08:35:21 +0000

    いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   resources :users
   resources :topics
   
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  
   get     '/login',  to: 'sessions#new'
   post    '/login',  to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
-  
 end
